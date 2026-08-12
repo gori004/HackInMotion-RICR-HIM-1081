b27643e6bdec0ee5ca627e5022b4d64a131e99c4
tanvijain1210 <tanvijain350@gmail.com>
2026-08-12 15:29:29 +0530

    added routes to authenticate user

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 
 // Sample Health Check Route
 app.get('/api/health', (req, res) => {
